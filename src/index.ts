63a4621547abd08faa03c1069223b4e8cd66b5ca
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-06-30 17:12:12 -0300

    feat: Added port and <T> type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {createPool, Pool, PoolConnection, RowDataPacket, ResultSetHeader} from 'mysql2/promise';
+import { createPool, Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 
 class Connection {
     private pool: Pool;
@@ -8,29 +8,27 @@ class Connection {
         private user: string,
         private password: string,
         private database: string,
-        private connectionLimit: number = 10
+        private connectionLimit: number = 10,
+        private port: number = 3306,
     ) {
         this.pool = createPool({
             host: this.host,
             user: this.user,
             password: this.password,
             database: this.database,
-            connectionLimit: this.connectionLimit
+            connectionLimit: this.connectionLimit,
+            port: this.port,
         });
     }
 
-    private async getConnection(): Promise<PoolConnection> {
-        return this.pool.getConnection();
-    }
-
-    private async query(sql: string, values?: any[]): Promise<ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | any> {
-        const connection = await this.getConnection();
+    async query<T extends RowDataPacket[] | RowDataPacket[][] | ResultSetHeader>(sql: string, values?: any[]): Promise<T> {
+        const connection = await this.pool.getConnection();
         try {
-            const [result] = await connection.query(sql, values);
-            return result;
+            const [rows] = await connection.query(sql, values);
+            return rows as T;
         } catch (error) {
             console.error('Error executing query:', error);
-            throw error; // Re-throw the error to handle it at a higher level
+            throw error;
         } finally {
             connection.release();
         }
@@ -41,22 +39,22 @@ class Connection {
         if (where) {
             sql += ` WHERE ${where}`;
         }
-        return await this.query(sql, params) as Promise<RowDataPacket[]>;
+        return this.query<RowDataPacket[]>(sql, params);
     }
 
     async insert(table: string, values: any): Promise<ResultSetHeader> {
         const sql = `INSERT INTO ${table} SET ?`;
-        return await this.query(sql, values) as Promise<ResultSetHeader>;
+        return this.query<ResultSetHeader>(sql, values);
     }
 
     async update(table: string, values: any, where: string, params?: any[]): Promise<ResultSetHeader> {
         const sql = `UPDATE ${table} SET ? WHERE ${where}`;
-        return this.query(sql, [values, ...(params || [])]) as Promise<ResultSetHeader>;
+        return this.query<ResultSetHeader>(sql, [values, ...(params || [])]);
     }
 
     async delete(table: string, where: string, params?: any[]): Promise<ResultSetHeader> {
         const sql = `DELETE FROM ${table} WHERE ${where}`;
-        return this.query(sql, params || []) as Promise<ResultSetHeader>;
+        return this.query<ResultSetHeader>(sql, params || []);
     }
 }
 
